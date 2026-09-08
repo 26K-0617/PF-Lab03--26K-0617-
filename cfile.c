0c0b44454c7b6794fe9f41b2d14275314565ef23
26K-0617 <rahmanazal2307@gmail.com>
2026-09-08 11:32:35 +0500

    Add files via upload

---
+++ cfile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int num1;
+	printf("Enter Your Number: ");
+	scanf("%d",&num1);
+	if(num1==0){
+		printf("Your Number is zero");
+	}
+	else{
+		printf("Your is non-zero");
+	}
+	return 0;
+}
